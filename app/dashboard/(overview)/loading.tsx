0ba1230da043609eb9ffdc5924068b1364791009
TitiphanPong <titiphan.pon@axonstech.com>
2025-07-02 01:33:22 +0700

    Chapter 11 Lesson

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from "../../ui/skeletons";
+
+export default function Loading() {
+    // Simulate a loading state for the dashboard
+  return <DashboardSkeleton />;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,8 @@ export default function Page() {
 					<AcmeLogo />
 				</div>
 				<div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-					<div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-						<div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
+					<div className="flex flex-col justify-center gap-4 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+						{/* <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" /> */}
 						<div className={styles.shape} />
 						<h1 className="text-2xl font-bold text-gray-800 md:text-4xl">
 							Next.js Learn Course
@@ -44,6 +44,13 @@ export default function Page() {
 							className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
 						>
 							<span>Log in</span>{" "}
+							<ArrowRightIcon className="w-5 md:w-6" />
+						</Link>
+
+						<Link href="/dashboard"
+							className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+						>
+							<span>Dashboard</span>{" "}
 							<ArrowRightIcon className="w-5 md:w-6" />
 						</Link>
 					</div>
